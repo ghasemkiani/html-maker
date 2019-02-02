a91705437d688fd47211fcf5bc8b3dfbd71a10e5
Ghasem Kiani <ghasemkiani@gmail.com>
2019-02-02 19:31:31 +0330

    Initial commit

---
+++ base.js
@@ -0,0 +1,16 @@
+//	@ghasemkiani/htmlmaker/base
+
+const base = require("@ghasemkiani/commonbase/base");
+const {cutil} = require("@ghasemkiani/commonbase/cutil");
+
+class Base extends base.Base {
+	render(wnode) {
+		//
+		return {wnode};
+	}
+}
+cutil.extend(Base.prototype, {
+	//
+});
+
+module.exports = {Base};
