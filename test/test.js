598e8855db43c7441a4461d1d3ea9d52430459b7
Ghasem Kiani <ghasemkiani@gmail.com>
2021-08-15 21:35:59 +0430

    Switch from mocha to ava for testing

--- test/test.js
+++ test/test.js
@@ -1,24 +1,18 @@
-const assert = require("assert");
-const {Page} = require("@ghasemkiani/htmlmaker/page");
+import test from "ava";
+import {renderable} from "../renderable.js";
+import {Component} from "../component.js";
+import {Page} from "../page.js";
+import {File} from "../file.js";
 
-describe("htmlmaker:Page", () => {
-	describe("#makeRelativeUri()", () => {
-		let page = new Page();
-		it("should create the correct relative URI", () => {
-			page.uri = "/";
-			assert.equal(page.makeRelativeUri("/books/sblp/"), "books/sblp/");
-		});
-		it("should create the correct relative URI", () => {
-			page.uri = "/translation";
-			assert.equal(page.makeRelativeUri("/books/sblp/"), "books/sblp/");
-		});
-		it("should create the correct relative URI", () => {
-			page.uri = "/translation/";
-			assert.equal(page.makeRelativeUri("/books/sblp/"), "../books/sblp/");
-		});
-		it("should create the correct relative URI", () => {
-			page.uri = "/translation/index.html";
-			assert.equal(page.makeRelativeUri("/books/sblp/"), "../books/sblp/");
-		});
-	});
+test("htmlmaker:Page#makeRelativeUri(): should create the correct relative URI", async t => {
+	let page = new Page();
+
+    page.uri = "/";
+    t.is(page.makeRelativeUri("/books/sblp/"), "books/sblp/");
+    page.uri = "/translation";
+    t.is(page.makeRelativeUri("/books/sblp/"), "books/sblp/");
+    page.uri = "/translation/";
+    t.is(page.makeRelativeUri("/books/sblp/"), "../books/sblp/");
+    page.uri = "/translation/index.html";
+    t.is(page.makeRelativeUri("/books/sblp/"), "../books/sblp/");
 });
