30fdf4f658a1c9f3c0a607ef66be246245dac196
Ghasem Kiani <ghasemkiani@gmail.com>
2024-01-27 17:37:05 +0330

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 export * from "./maker.js";
 export * from "./host.js";
-export * from "./html.js";
+// export * from "./html.js";
 export * from "./xhtml.js";
-export * from "./bootstrap.js";
+// export * from "./bootstrap.js";
 export * from "./xbootstrap.js";
